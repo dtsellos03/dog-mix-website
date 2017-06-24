4e9937962464bf1636162979ccffaf5aed063f21
dtsellos03 <dtsellos03@gmail.com>
2017-06-24 02:22:53 +0000

    Edited database env variable

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ var mixesRoutes = require ('./routes/mixes');
 
 
 var app = express();
-mongoose.connect('mongodb://localhost/dogalchemy');
-seedDB()
+mongoose.connect(process.env.DATABASEURL);
+//seedDB()
 
 
 
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/mixes', mixesRoutes)
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   return res.render('index')
 });
