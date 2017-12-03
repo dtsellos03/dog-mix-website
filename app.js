943664f115ae9299d092493dccb109d8751fe7a4
dtsellos03 <dtsellos03@gmail.com>
2017-12-02 17:56:50 -0800

    updated keys

--- app.js
+++ app.js
@@ -5,19 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var seedDB      = require("./seeds");
-
+var seedDB = require("./seeds");
+var db_key = require('./db_key.js')
 var index = require('./routes/index');
-var mixesRoutes = require ('./routes/mixes');
-
-
-
+var mixesRoutes = require('./routes/mixes');
 
 var app = express();
-mongoose.connect(process.env.DATABASEURL);
+console.log(db_key);
+mongoose.connect('mongodb://doguser:doguser@ds135552.mlab.com:35552/dogalchemy');
 //seedDB()
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,13 +23,12 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/getMixes', mixesRoutes)
-
 
 app.use(function(req, res, next) {
   return res.render('index')
